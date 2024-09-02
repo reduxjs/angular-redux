61a27bc6decc9ca7890cf5322e968ef8af78d066
Corbin Crutchley <git@crutchcorn.dev>
2024-09-02 04:40:11 -0700

    chore: more work to scaffold angular testing

--- projects/angular-redux/src/tests/test.spec.ts
+++ projects/angular-redux/src/tests/test.spec.ts
@@ -1,3 +1,15 @@
-it('Should be true', () => {
-  expect(true).toBe(true)
+import { Component } from '@angular/core'
+import { render } from '@testing-library/angular'
+import '@testing-library/jest-dom'
+
+@Component({
+  selector: "app-root",
+  standalone: true,
+  template: "<p>Testing</p>"
+})
+class TestSpec {}
+
+it('Should render a component', async () => {
+  const {getByText} = await render(TestSpec)
+  expect(getByText("Testing")).toBeInTheDocument();
 })
