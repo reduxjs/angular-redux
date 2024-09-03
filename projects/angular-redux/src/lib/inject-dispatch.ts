0a087f6f48bdbd6b8963bb94e47d88a8d1d04393
Corbin Crutchley <git@crutchcorn.dev>
2024-09-03 00:39:34 -0700

    feat: add support for withTypes

--- projects/angular-redux/src/lib/inject-dispatch.ts
+++ projects/angular-redux/src/lib/inject-dispatch.ts
@@ -1,10 +1,91 @@
-import type { Dispatch, UnknownAction } from 'redux'
+import type { Dispatch, UnknownAction, Action } from 'redux'
 import { assertInInjectionContext } from '@angular/core'
 import { injectStore } from './inject-store'
 
-// TODO: Add `withTypes` support
-export function injectDispatch<AppDispatch extends Dispatch<UnknownAction> = Dispatch<UnknownAction>>(): AppDispatch {
-  assertInInjectionContext(injectDispatch)
-  const store = injectStore();
-  return store.dispatch as AppDispatch
+
+/**
+ * Represents a custom injection that provides a dispatch function
+ * from the Redux store.
+ *
+ * @template DispatchType - The specific type of the dispatch function.
+ *
+ * @public
+ */
+export interface InjectDispatch<
+  DispatchType extends Dispatch<UnknownAction> = Dispatch<UnknownAction>,
+> {
+  /**
+   * Returns the dispatch function from the Redux store.
+   *
+   * @returns The dispatch function from the Redux store.
+   *
+   * @template AppDispatch - The specific type of the dispatch function.
+   */
+    <AppDispatch extends DispatchType = DispatchType>(): AppDispatch
+
+  /**
+   * Creates a "pre-typed" version of {@linkcode injectDispatch injectDispatch}
+   * where the type of the `dispatch` function is predefined.
+   *
+   * This allows you to set the `dispatch` type once, eliminating the need to
+   * specify it with every {@linkcode injectDispatch injectDispatch} call.
+   *
+   * @returns A pre-typed `injectDispatch` with the dispatch type already defined.
+   *
+   * @example
+   * ```ts
+   * export const injectAppDispatch = injectDispatch.withTypes<AppDispatch>()
+   * ```
+   *
+   * @template OverrideDispatchType - The specific type of the dispatch function.
+   */
+  withTypes: <
+    OverrideDispatchType extends DispatchType,
+  >() => InjectDispatch<OverrideDispatchType>
 }
+
+/**
+ * Injection factory, which creates a `injectDispatch` injection bound to a given context.
+ *
+ * @returns {Function} A `injectDispatch` injection bound to the specified context.
+ */
+export function createDispatchInjection<
+  ActionType extends Action = UnknownAction,
+>() {
+  const injectDispatch = <AppDispatch extends Dispatch<UnknownAction> = Dispatch<UnknownAction>>(): AppDispatch  => {
+    assertInInjectionContext(injectDispatch)
+    const store = injectStore();
+    return store.dispatch as AppDispatch
+  }
+
+  Object.assign(injectDispatch, {
+    withTypes: () => injectDispatch,
+  })
+
+  return injectDispatch as InjectDispatch<Dispatch<ActionType>>
+}
+
+/**
+ * A injection to access the redux `dispatch` function.
+ *
+ * @returns {any|function} redux store's `dispatch` function
+ *
+ * @example
+ *
+ * import { injectDispatch } from 'angular-redux'
+ *
+ * @Component({
+ *   selector: 'example-component',
+ *   template: `
+ *     <div>
+ *       <span>{{value}}</span>
+ *       <button (click)="increaseCounter()">Increase counter</button>
+ *     </div>
+ *   `
+ * })
+ * export class CounterComponent {
+ *   dispatch = injectDispatch()
+ *   increaseCounter = () => dispatch({ type: 'increase-counter' })
+ * }
+ */
+export const injectDispatch = /* #__PURE__*/ createDispatchInjection()
--- projects/angular-redux/src/lib/inject-store.ts
+++ projects/angular-redux/src/lib/inject-store.ts
@@ -1,10 +1,106 @@
 import { assertInInjectionContext, inject } from '@angular/core'
 import { ReduxProvider } from './provider'
-import type { Store, Action, UnknownAction } from 'redux'
+import type { Store, Action } from 'redux'
 
-export function injectStore<A extends Action<string> = UnknownAction, S = unknown>(): Store<S, A> {
-  assertInInjectionContext(injectStore)
-  const context = inject(ReduxProvider)
-  const { store } = context
-  return store
+/**
+ * Represents a type that extracts the action type from a given Redux store.
+ *
+ * @template StoreType - The specific type of the Redux store.
+ *
+ * @internal
+ */
+export type ExtractStoreActionType<StoreType extends Store> =
+  StoreType extends Store<any, infer ActionType> ? ActionType : never
+
+/**
+ * Represents a custom injection that provides access to the Redux store.
+ *
+ * @template StoreType - The specific type of the Redux store that gets returned.
+ *
+ * @public
+ */
+export interface InjectStore<StoreType extends Store> {
+  /**
+   * Returns the Redux store instance.
+   *
+   * @returns The Redux store instance.
+   */
+  (): StoreType
+
+  /**
+   * Returns the Redux store instance with specific state and action types.
+   *
+   * @returns The Redux store with the specified state and action types.
+   *
+   * @template StateType - The specific type of the state used in the store.
+   * @template ActionType - The specific type of the actions used in the store.
+   */
+    <
+      StateType extends ReturnType<StoreType['getState']> = ReturnType<
+        StoreType['getState']
+      >,
+      ActionType extends Action = ExtractStoreActionType<Store>,
+    >(): Store<StateType, ActionType>
+
+  /**
+   * Creates a "pre-typed" version of {@linkcode injectStore injectStore}
+   * where the type of the Redux `store` is predefined.
+   *
+   * This allows you to set the `store` type once, eliminating the need to
+   * specify it with every {@linkcode injectStore injectStore} call.
+   *
+   * @returns A pre-typed `injectStore` with the store type already defined.
+   *
+   * @example
+   * ```ts
+   * export const useAppStore = injectStore.withTypes<AppStore>()
+   * ```
+   *
+   * @template OverrideStoreType - The specific type of the Redux store that gets returned.
+   */
+  withTypes: <
+    OverrideStoreType extends StoreType,
+  >() => InjectStore<OverrideStoreType>
 }
+
+/**
+ * Injection factory, which creates a `injectStore` injection bound to a given context.
+ *
+ * @returns {Function} A `injectStore` injection bound to the specified context.
+ */
+export function createStoreInjection<
+  StateType = unknown,
+  ActionType extends Action = Action,
+>() {
+  const injectStore = () => {
+    assertInInjectionContext(injectStore)
+    const context = inject(ReduxProvider)
+    const { store } = context
+    return store
+  }
+
+  Object.assign(injectStore, {
+    withTypes: () => injectStore,
+  })
+
+  return injectStore as InjectStore<Store<StateType, ActionType>>
+}
+
+/**
+ * A injection to access the redux store.
+ *
+ * @returns {any} the redux store
+ *
+ * @example
+ *
+ * import { injectStore } from 'angular-redux'
+ *
+ * @Component({
+ *   selector: 'example-component',
+ *   template: `<div>{{store.getState()}}</div>`
+ * })
+ * export class CounterComponent {
+ *   store = injectStore()
+ * }
+ */
+export const injectStore = /* #__PURE__*/ createStoreInjection()
